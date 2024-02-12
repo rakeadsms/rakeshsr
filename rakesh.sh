3a401c8ac8817764f4245a511554129896b48d26
root <root@ip-172-31-12-199.sa-east-1.compute.internal>
2024-02-12 14:23:31 +0000

    shkd

--- rakesh.sh
+++ rakesh.sh
@@ -1,4 +1,27 @@
 chmod 400 "rr.pem"ksdjcmsldc ksm dcls csd lscm lsmwdklmdc
 sdcslmdcs;dc,sa'dsc
 sadcsdlcsd;c,sdc ksd clsdcklsdmcls
+asmclkamsklamlxamxlamAs
+ca
+s
+ad
+ad
+a
+ad
+da
+da
+
+da
+da
+
+ad
+da
+da
+adad
+
+da
+da
+ad
+
+ad
 
